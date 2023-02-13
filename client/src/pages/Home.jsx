a07a8eb1055a3126fea241b9109b4e88579baf1c
sleep9ull <sleep9ull@gmail.com>
2023-02-13 23:32:46 +0800

    change backend_url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true)
       try {
-        const response = await fetch('http://localhost:8080/api/v1/posts', {
+        const response = await fetch('https://clone-dalle.onrender.com/api/v1/posts', {
           method: 'GET',
           header: {
             'Content-Type': 'application/json',
